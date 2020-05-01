df1dad7e1081e734d7daf9d3e1128485f6a7895f
NikhilReddy-1 <nikhilreddy1568@gmail.com>
2020-05-01 17:33:12 +0530

    blank mail

---
+++ mail-file.py
@@ -0,0 +1,22 @@
+import os
+import smtplib
+
+EMAIL_ADD = os.environ.get('DB_USER')
+EMAIL_PASS = os.environ.get('DB_PASS')
+
+
+
+
+with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login(EMAIL_ADD,EMAIL_PASS)
+
+    subject = 'COVID-19 STATS'
+    body = 'These are the stats for your area'
+
+    msg = f'Subject: {subject}\n\n{body}'
+
+    smtp.sendmail(EMAIL_ADD, 'nikhilreddy156@gmail.com', msg)
