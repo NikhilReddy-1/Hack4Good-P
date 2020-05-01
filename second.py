dcf173207945b0b16db28ee6373dfe78dac90aa8
NikhilReddy-1 <63509209+NikhilReddy-1@users.noreply.github.com>
2020-05-01 21:36:39 +0530

    Add files via upload
    Final

--- second.py
+++ second.py
@@ -3,8 +3,26 @@ from flask import Flask, request,jsonify
 import json
 import matplotlib.pyplot as plt
 import numpy
+import os
+import smtplib
+
+global con
+global rec
+global act
+global dec
+global ip2
+global ip1
+global total
+global red_zone
 
 
+
+EMAIL_ID = os.environ.get('DB_USER')
+EMAIL_PASS = os.environ.get('DB_PASS')
+
+
+
+RECEIVER = input("Enter your E-mail ID:\n")
 
 def getResponse(url):
     openUrl = urllib.request.urlopen(url)
@@ -16,15 +34,22 @@ def getResponse(url):
     return jsonData
 
 def main():
-    
+
+    total = 0
+
+
     y_ax = []
     x_ax = []
     y = {}
+
 
     urlData = "https://api.covid19india.org/v2/state_district_wise.json"
     jsonData = getResponse(urlData)
     ip1 = input("Enter your state: ")
     ip2 = input("Enter your district: ")
+
+
+
     for i in jsonData: 
         x = i["state"]
         if x == ip1 :
@@ -40,17 +65,58 @@ def main():
                     print("Recovered Patients : ",rec)
                     print("Active Patients : ",act)
                     print("Deceased Patients : ",dec)
-    if act >= 15 :
-        print("Your area is in Red Zone!")
-        
-    #print(y)
-    #print("",type(y))
+
+
+    for i in jsonData:
+        x = i["state"]
+        y[x] = i["districtData"]
+        for t in y[x]:
+            con = t["confirmed"]
+            total = total + con
+
+
+
+
+
+
+    with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+
+
+        smtp.login(EMAIL_ID,EMAIL_PASS)
+        subject = 'COVID-19 STATS UPDATE'
+
+        data_string1 = f'Dear user!\n' \
+                      f'We hope you\'re staying safe and staying home.\n' \
+                      f'Currently, more than 3 million people are affected worldwide and {total} people are affected in India.\n' \
+                      f'Here are the stats for {ip2},{ip1} -\n' \
+                      f'Confirmed Patients : {con}\n' \
+                      f'Recovered Patients : {rec}\n' \
+                      f'Active Patients : {act}\n' \
+                      f'Deceased Patients : {dec}\n' \
+                       f'Your area is in Red Zone!' \
+
+
+        data_string = f'Dear user!\n' \
+                  f'We hope you\'re staying safe and staying home.\n' \
+                  f' Currently, more than 3 million people are affected worldwide and {total} people are affected in India.\n' \
+                  f'Here are the stats for {ip2},{ip1} -\n' \
+                  f'Confirmed Patients : {con}\n' \
+                  f'Recovered Patients : {rec}\n' \
+                  f'Active Patients : {act}\n' \
+                  f'Deceased Patients : {dec}\n' \
+                      f'Your area is not in Red Zone!' \
+
+
+        if act >= 15:
+            body = data_string1
+        else:
+            body = data_string
+
+        msg = f'Subject: {subject}\n\n{body}'
+
+        smtp.sendmail(EMAIL_ID, RECEIVER, msg)
     
-        
-    
-    
-    
-    
+
     
 
 if __name__ == '__main__':
